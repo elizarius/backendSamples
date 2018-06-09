7c7f86cd98591301a09d896596ef98b67709d316
Alexander Elizarov <al.elizarov@gmail.com>
2018-06-09 13:08:23 +0300

    Initial commit for backend examples
    Signed-off-by: Alexander Elizarov <al.elizarov@gmail.com>

---
+++ java/jaxrs/endpoint-app/src/main/java/aelz/home/MyApplication.java
@@ -0,0 +1,11 @@
+package aelz.home;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author
+ */
+@ApplicationPath("webresources")
+public class MyApplication extends Application {
+}
