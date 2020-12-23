bc0ad48bcab575ef38eb613d184943a27229113b
Alexander Elizarov <alexander.elizarov@ericsson.com>
2020-12-23 16:45:39 +0200

    ss: http_requests
    Signed-off-by: Alexander Elizarov <alexander.elizarov@ericsson.com>

---
+++ crypto/built_hash.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+val = 100
+aelz ="aelz aelz aelz"
+
+print(hash(val))
+print(hash(aelz))
+print(hash("aelz aelz aelz"))
+
--- python/http_reqs_ss.py
+++ python/http_reqs_ss.py
@@ -14,11 +14,13 @@ data =json.loads(resp.text)
 print('\nData:\n')
 pprint(data)
 
-print('\nGet request with parameters AKA web browsing ***\n')
+print('\n***** GET request  AKA web browsing ***\n')
 resp = requests.get('https://www.wikipedia.org/', verify=False)
 print('WIKI URL:  {}'.format(resp.url))
 webbrowser.open(resp.url)
 
+
+print('\n***** GET request with parameters ***\n')
 pars ={'q' : 'alexander tikhonov biathlon'}
 resp = requests.get(url='https://www.youtube.com/search',
                     params=pars, 
@@ -27,10 +29,35 @@ print('search URL:  {}'.format(resp.url))
 webbrowser.open(resp.url)
 
 
-print('\nPOST request ***\n')
+print('\n***** POST request ***\n')
 resp = requests.post('https://www.wikipedia.org/w/index.php', 
                     data={'search' : 'skillsoft'},
                     verify=False)
+#pprint(resp.text)
+
+
+print('\n***** HEAD request ***\n')
+resp = requests.head('http://example.com')
+pprint(resp.status_code)
+pprint(resp.content)
+pprint(resp.headers)
+
+print('\n***** HEAD request 2 ***\n')
+resp = requests.head('http://alexander-tikhonov.ru')
+pprint(resp.status_code)
+pprint(resp.content)
+pprint(resp.headers)
+pprint(resp.headers['Set-Cookie'])
+
+
+print('\n***** OPTIONS request, see access control methods ***\n')
+resp = requests.options('https://httpbin.org/get')
+#resp = requests.head('http://alexander-tikhonov.ru')
+pprint(type(resp))
+pprint(resp.headers)
+
+
+print('\n***** DELETE request EXAMPLE  ***\n')
+resp = requests.options('https://httpbin.org/delete')
 pprint(resp.text)
 
-               
