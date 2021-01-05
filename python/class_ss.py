e84a61a152e062792636ad432c763b059685ce64
Alexander Elizarov <alexander.elizarov@ericsson.com>
2021-01-05 16:51:43 +0200

    ss: bacic inheritance and polimorphism
    Signed-off-by: Alexander Elizarov <alexander.elizarov@ericsson.com>

--- python/class_ss.py
+++ python/class_ss.py
@@ -5,7 +5,7 @@ Terminology:
  
 - Object:       everything in python is object 
 - Type:         type of object type(x)
-- Attribute:    data members of type or class dir(X) in terms C++   --> hasattr
+- Attribute:    data members of type or class dir(X) in terms C++  --> hasattr
 - Properties:   special attributes, like  __get__, __set__, __delete__ , 
                 f.i. used to limit access to  attributes if want to make attr private
 - Method:       data functions in terms C++ 
@@ -29,7 +29,8 @@ class Animal(object):
     def get_name(self):
         return(self.name)
 
-    @classmethod
+    # classmethod works on level of class but not instance
+    @classmethod  
     def print_animal(cls):
         print('ANIMAL FUNCTION')
 
@@ -47,7 +48,7 @@ print('\n** Type int: {} '.format(type(a)))
 print('\n** Type list: {} '.format(type([])))
 
 print('** Type of Animal instance: {} '.format(type(an)))
-print('** Type of Animal instance: {} '.format(type(Animal)))
+print('** Type of Animal class: {} '.format(type(Animal)))
 print('** Type of Animal class name: {} '.format(an.__class__))
 print('\n** Convert Animal class to dict: {} '.format(an.__dict__))
 print('\n** Animal Other attr: {} '.format(an.__module__))
@@ -57,26 +58,76 @@ print('** Animal instance name: {} '.format(an))
 
 # 3. Class initialization
 
-# Note: @classmethod / cls used to implemented inheritnce
+# Note: @classmethod / cls used to implemented inheritance
 print('\n***    Age,, global class member: {} '.format(an.age))
 
 
 # 4. Class inheritance
-print('\n\n ************ Class Inheritance Demo **********')
-
+print('\n\n ************  Class Inheritance Demo **********')
 
 class Horse(Animal):
 
     def __init__(self , legs=4):
         self.legs = legs
         self.name = 'DIMKABUKASUKA'
+        print('HORSE init called')
+        # Base class init is not called by default. Could be:
+        # Animal.__init(self, ...)
 
     def __str__(self):
         return('AELZ Horse inherited')
 
-horse = Horse(legs=8)                           # base class init is not called
+horse = Horse(legs=8)                                       # base class init is not called by default
 print('** Inheritance: {} '.format(horse))
 print('** N of legs: {} '.format(horse.legs))
-print('** Horse age: {} '.format(horse.age))    # from global class method
-print('** Call parent: {} '.format(horse.print_animal()))
-print('** Horse name : {} '.format(horse.get_name()))    # from global class method
+print('** Horse age: {} '.format(horse.age))                # global class static attribute 
+horse.print_animal()
+print('** Horse name : {} '.format(horse.get_name()))       # function from global class method
+
+
+print('\n** Horse hierarchy help function ** \n')
+#help(horse)
+print('\n** Subclass checking: {} \n'.format(issubclass(Horse, Animal)))
+
+
+
+print('\n\n ************  Multiple Inheritance Demo **********')
+class Father:
+    pass
+
+class Mother:
+    pass
+
+class Child1(Father, Mother):
+    pass
+
+#help(Child1)
+
+
+print('\n\n ************  Polymorphism Demo: function overriding **********')
+class Base:
+    def print_method(self):
+        print('I am BASE')
+
+    def print_base(self):
+        print('I am BASE BASE BASE')
+
+
+class Der1(Base):
+    def print_method(self):
+        print('I am DER_111')
+
+
+class Der2(Base):
+    def print_method(self):
+        print('I am DER_222')
+
+bas = Base()
+bas.print_method()
+d1 = Der1()
+d1.print_method()
+d2 = Der2()
+d2.print_method()
+d2.print_base()
+
+
