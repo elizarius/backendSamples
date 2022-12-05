feb45d80e44985c9716e599bf3d5b9fcef210189
Alexander Elizarov <alexander.elizarov@ericsson.com>
2022-12-05 15:58:27 +0200

    json example
    Signed-off-by: Alexander Elizarov <alexander.elizarov@ericsson.com>

--- python/jsonLoads.py
+++ python/jsonLoads.py
@@ -1,33 +1,17 @@
 #!/usr/bin/env python
-#import os
-#import sys
 import json
 
-vdc_list = ['vdc_1','vdc_2']
-ha = 'xaxaxaxa'
-json_data = json.dumps(vdc_list)
+# https://www.educative.io/answers/what-is-the-difference-between-jsonloads-and-jsondumps
+# resp.text
+# resp.json()))   # actually the same as json.loads()
 
-print ("AELZ_01 vdcs {}".format(vdc_list))
-print ("AELZ_01 j_data {}".format(json_data))
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+print ("AELZ_01 string  {}".format(x))
+#print(x["age"])    NOK , because string 
+y = json.loads(x)
+print(y["age"])
 
-print ("AELZ_03 ha {}".format(ha))
-print ("AELZ_03 j_ha {}".format(json.dumps(ha)))
-
-
-
-
-print ("")
-print ("*********************************")
-print ("")
-
-
-print ("AELZ_04 l_data {}".format(json.loads(json_data)))
-print ("AELZ_05 l_ha {}".format(json.loads(json.dumps(ha))))
-
-#vdcs = json.loads(ecm_data['vdc'])
-
-#print ("AELZ_01 vdcs {}".format(vdcs))
-#for vdc in vdcs:
-#    print ("AELZ_02 vdc {}".format(vdc))
-
+z = json.dumps(y)
+print ("AELZ_02 dict  {}".format(y))
+print ("AELZ_03 string  {}".format(z))
 
