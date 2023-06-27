1c313db135f953e8cd3bd46e4b41dda3a18155e1
Alexander Elizarov <alexander.elizarov@ericsson.com>
2023-06-27 11:44:00 +0300

    redis: add simple celery and redis examples
    Signed-off-by: Alexander Elizarov <alexander.elizarov@ericsson.com>

--- redis/celery_app/celery_app/celeryconfig.py
+++ redis/celery_app/celery_app/celeryconfig.py
@@ -1,5 +1,5 @@
-broker_url: str = "redis://127.0.0.1:6379/0"
-result_backend: str = "redis://127.0.0.1:6379/0"
+broker_url: str = "redis://100.76.169.30:6379/0"
+result_backend: str = "redis://100.76.169.30:6379/0"
 task_default_queue: str = "Project-Celery-App"
 imports: tuple = (
     "celery_app.tasks",
